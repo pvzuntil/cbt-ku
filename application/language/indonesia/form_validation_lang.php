1bb2ca7bfb49dd8f3b43c36f7d562cfd729a9fd8
untillNess <pvzuntil@gmail.com>
2020-05-27 14:11:46 +0700

    Add Reset Password

--- application/language/indonesia/form_validation_lang.php
+++ application/language/indonesia/form_validation_lang.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -35,34 +36,34 @@
  * @since	Version 1.0.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['form_validation_required']		= '{field} tidak boleh kosong.';
-$lang['form_validation_isset']			= '{field} harus memiliki nilai.';
-$lang['form_validation_valid_email']		= '{field} harus berisi alamat email yang benar.';
-$lang['form_validation_valid_emails']		= '{field} harus berisi alamat email yang benar.';
-$lang['form_validation_valid_url']		= 'The {field} field must contain a valid URL.';
-$lang['form_validation_valid_ip']		= 'The {field} field must contain a valid IP.';
-$lang['form_validation_min_length']		= 'The {field} field must be at least {param} characters in length.';
-$lang['form_validation_max_length']		= 'The {field} field cannot exceed {param} characters in length.';
-$lang['form_validation_exact_length']		= 'The {field} field must be exactly {param} characters in length.';
-$lang['form_validation_alpha']			= 'The {field} field may only contain alphabetical characters.';
-$lang['form_validation_alpha_numeric']		= 'The {field} field may only contain alpha-numeric characters.';
-$lang['form_validation_alpha_numeric_spaces']	= 'The {field} field may only contain alpha-numeric characters and spaces.';
-$lang['form_validation_alpha_dash']		= 'The {field} field may only contain alpha-numeric characters, underscores, and dashes.';
-$lang['form_validation_numeric']		= '{field} hanya boleh berisi angka.';
-$lang['form_validation_is_numeric']		= '{field} harus berisi karakter angka.';
-$lang['form_validation_integer']		= '{field} harus berisi integer.';
-$lang['form_validation_regex_match']		= 'The {field} field is not in the correct format.';
-$lang['form_validation_matches']		= 'The {field} field does not match the {param} field.';
-$lang['form_validation_differs']		= 'The {field} field must differ from the {param} field.';
-$lang['form_validation_is_unique'] 		= 'The {field} field must contain a unique value.';
-$lang['form_validation_is_natural']		= 'The {field} field must only contain digits.';
-$lang['form_validation_is_natural_no_zero']	= 'The {field} field must only contain digits and must be greater than zero.';
-$lang['form_validation_decimal']		= '{field} harus berisi angka desimal.';
-$lang['form_validation_less_than']		= '{field} harus berisi angka kurang dari {param}.';
-$lang['form_validation_less_than_equal_to']	= '{field} harus berisi angka kurang dari sama dengan {param}.';
-$lang['form_validation_greater_than']		= '{field} harus berisi angka yang lebih besar dari {param}.';
-$lang['form_validation_greater_than_equal_to']	= 'The {field} field must contain a number greater than or equal to {param}.';
-$lang['form_validation_error_message_not_set']	= 'Unable to access an error message corresponding to your field name {field}.';
-$lang['form_validation_in_list']		= 'The {field} field must be one of: {param}.';
+$lang['form_validation_required']        = '{field} tidak boleh kosong.';
+$lang['form_validation_isset']            = '{field} harus memiliki nilai.';
+$lang['form_validation_valid_email']        = '{field} harus berisi alamat email yang benar.';
+$lang['form_validation_valid_emails']        = '{field} harus berisi alamat email yang benar.';
+$lang['form_validation_valid_url']        = 'The {field} field must contain a valid URL.';
+$lang['form_validation_valid_ip']        = 'The {field} field must contain a valid IP.';
+$lang['form_validation_min_length']        = '{field} harus di isi minimal {param} karakter';
+$lang['form_validation_max_length']        = 'The {field} field cannot exceed {param} characters in length.';
+$lang['form_validation_exact_length']        = 'The {field} field must be exactly {param} characters in length.';
+$lang['form_validation_alpha']            = 'The {field} field may only contain alphabetical characters.';
+$lang['form_validation_alpha_numeric']        = 'The {field} field may only contain alpha-numeric characters.';
+$lang['form_validation_alpha_numeric_spaces']    = 'The {field} field may only contain alpha-numeric characters and spaces.';
+$lang['form_validation_alpha_dash']        = 'The {field} field may only contain alpha-numeric characters, underscores, and dashes.';
+$lang['form_validation_numeric']        = '{field} hanya boleh berisi angka.';
+$lang['form_validation_is_numeric']        = '{field} harus berisi karakter angka.';
+$lang['form_validation_integer']        = '{field} harus berisi integer.';
+$lang['form_validation_regex_match']        = 'The {field} field is not in the correct format.';
+$lang['form_validation_matches']        = '{field} harus sama dengan {param}';
+$lang['form_validation_differs']        = 'The {field} field must differ from the {param} field.';
+$lang['form_validation_is_unique']         = 'The {field} field must contain a unique value.';
+$lang['form_validation_is_natural']        = 'The {field} field must only contain digits.';
+$lang['form_validation_is_natural_no_zero']    = 'The {field} field must only contain digits and must be greater than zero.';
+$lang['form_validation_decimal']        = '{field} harus berisi angka desimal.';
+$lang['form_validation_less_than']        = '{field} harus berisi angka kurang dari {param}.';
+$lang['form_validation_less_than_equal_to']    = '{field} harus berisi angka kurang dari sama dengan {param}.';
+$lang['form_validation_greater_than']        = '{field} harus berisi angka yang lebih besar dari {param}.';
+$lang['form_validation_greater_than_equal_to']    = 'The {field} field must contain a number greater than or equal to {param}.';
+$lang['form_validation_error_message_not_set']    = 'Unable to access an error message corresponding to your field name {field}.';
+$lang['form_validation_in_list']        = 'The {field} field must be one of: {param}.';
