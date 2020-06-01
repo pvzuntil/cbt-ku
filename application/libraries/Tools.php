c46babbc1fdade20aa1449d5bcf915b06530f840
untillNess <pvzuntil@gmail.com>
2020-06-01 11:12:36 +0700

    Update Tools.php

--- application/libraries/Tools.php
+++ application/libraries/Tools.php
@@ -36,11 +36,41 @@ class Tools
 
 	function getAlpha($index)
 	{
-		$alpha = [
-			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-			'Z'
-		];
+		$alpha = $this->createColumnsArray('ZZ');
 
 		return $alpha[$index];
+	}
+
+	function createColumnsArray($end_column, $first_letters = '')
+	{
+		$columns = array();
+		$length = strlen($end_column);
+		$letters = range('A', 'Z');
+
+		// Iterate over 26 letters.
+		foreach ($letters as $letter) {
+			// Paste the $first_letters before the next.
+			$column = $first_letters . $letter;
+
+			// Add the column to the final array.
+			$columns[] = $column;
+
+			// If it was the end column that was added, return the columns.
+			if ($column == $end_column)
+				return $columns;
+		}
+
+		// Add the column children.
+		foreach ($columns as $column) {
+			// Don't itterate if the $end_column was already set in a previous itteration.
+			// Stop iterating if you've reached the maximum character length.
+			if (!in_array($end_column, $columns) && strlen($column) < $length) {
+				$new_columns = $this->createColumnsArray($end_column, $column);
+				// Merge the new columns which were created with the final columns array.
+				$columns = array_merge($columns, $new_columns);
+			}
+		}
+
+		return $columns;
 	}
 }
