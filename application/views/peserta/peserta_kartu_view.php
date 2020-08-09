4264b63dba92160ffd201d40d20fb681cea67724
pvzuntil <pvzuntil@gmail.com>
2020-08-09 10:37:34 +0700

    redesign  peserta kartu

--- application/views/peserta/peserta_kartu_view.php
+++ application/views/peserta/peserta_kartu_view.php
@@ -1,62 +1,59 @@
 <!-- Content Header (Page header) -->
 <section class="content-header">
-    <h1>
-        Cetak Kartu Peserta
-        <small>Mencetak Kartu Peserta berdasarkan data Peserta yang telah di inputkan.</small>
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="<?php echo site_url(); ?>/"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Kartu Peserta</li>
-    </ol>
+    <div class="container-fluid">
+        <h1>
+            Cetak Kartu Peserta
+        </h1>
+    </div>
 </section>
 
 <!-- Main content -->
 <section class="content">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="callout callout-info">
-                <h4>Informasi</h4>
-                <p>Lakukan Pengaturan nama pelaksana sebelum mencetak kartu peserta agar kartu sesuai dengan Organisasi tempat anda melaksanakan Tes.</p>
-                <p>Pastikan terlebih dahulu data Group dan data Peserta sudah ter-upload.</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="callout callout-info">
+                    <h4>Informasi</h4>
+                    <p>Lakukan Pengaturan nama pelaksana sebelum mencetak kartu peserta agar kartu sesuai dengan Organisasi tempat anda melaksanakan Tes.</p>
+                    <p>Pastikan terlebih dahulu data Group dan data Peserta sudah ter-upload.</p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <?php echo form_open($url . '/kartu', 'id="form-kartu"'); ?>
-            <div class="box">
-                <div class="box-header with-border">
-                    <div class="box-title">Cetak Kartu Peserta</div>
-                </div><!-- /.box-header -->
+        <div class="row">
+            <div class="col-12">
+                <?php echo form_open($url . '/kartu', 'id="form-kartu"'); ?>
+                <div class="card">
+                    <div class="card-header with-border">
+                        <div class="card-title">Cetak Kartu Peserta</div>
+                    </div><!-- /.card-header -->
 
-                <div class="box-body">
-                    <div class="form-group col-sm-6">
-                        <label>Pilih Group Peserta</label>
-                        <div id="data-group">
-                            <select name="group" id="group" class="form-control input-sm">
-                                <?php if (!empty($select_group)) {
-                                    echo $select_group;
-                                } ?>
-                            </select>
+                    <div class="card-body">
+                        <div class="form-group col-sm-6">
+                            <label>Pilih Group Peserta</label>
+                            <div id="data-group">
+                                <select name="group" id="group" class="form-control input-sm">
+                                    <?php if (!empty($select_group)) {
+                                        echo $select_group;
+                                    } ?>
+                                </select>
+                            </div>
+                            <small class="help-block text-muted">Pilih Data Group Peserta yang akan di cetak</small>
                         </div>
-                        <p class="help-block">Pilih Data Group Peserta yang akan di cetak</p>
+
+                        <?php if (!empty($hasil)) {
+                            echo $hasil;
+                        } ?>
                     </div>
 
-                    <?php if (!empty($hasil)) {
-                        echo $hasil;
-                    } ?>
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-primary" id="kartu">Cetak kartu</button>
+                    </div>
                 </div>
-
-                <div class="box-footer">
-                    <button type="button" class="btn btn-primary" id="kartu">Cetak kartu</button>
-                </div>
+                <?php echo form_close(); ?>
             </div>
-            <?php echo form_close(); ?>
         </div>
     </div>
 </section><!-- /.content -->
-
-
 
 <script lang="javascript">
     $(function() {
